c5db1a7469880ec1cfa6077d642cf591ea397a01
ilaumjd <ilaumjd@gmail.com>
2025-02-13 22:17:52 +0700

    void: niri fix

--- iam/scripts/nirirc.sh
+++ iam/scripts/nirirc.sh
@@ -1,3 +1,3 @@
 if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then
-  exec niri --session
+  dbus-run-session niri --session
 fi
---
+++ iam/scripts/voidrc.sh
@@ -0,0 +1,1 @@
+export XDG_DATA_DIRS=/usr/share:/usr/local/share:/home/iam/.local/share:/home/iam/.nix-profile/share:/var/lib/flatpak/exports/share:/home/iam/.local/share/flatpak/exports/share
