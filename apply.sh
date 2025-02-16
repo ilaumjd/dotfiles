5888fbe249b64bb2f3f127ccdd54e1c1272d8e16
ilaumjd <ilaumjd@gmail.com>
2025-02-16 09:00:39 +0700

    labwc: setup

--- apply.sh
+++ apply.sh
@@ -16,6 +16,7 @@ elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
   # wayland
   ln -s "$DOTFILES_DIR/hypr" ~/.config/hypr
+  ln -s "$DOTFILES_DIR/labwc" ~/.config/labwc
   ln -s "$DOTFILES_DIR/niri" ~/.config/niri
   ln -s "$DOTFILES_DIR/sway" ~/.config/sway
 
