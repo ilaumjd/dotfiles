8c659a8ef3df39d21da69445e97c14f8943b7bc3
ilaumjd <ilaumjd@gmail.com>
2025-08-18 21:45:34 +0700

    stow: create linux directory

--- apply.sh
+++ apply.sh
@@ -1,30 +1,9 @@
 #!/bin/bash
 
-DOTFILES_DIR="$(dirname "$(readlink -f "$0")")"
-
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  ln -s "$DOTFILES_DIR/nvim" ~/.config/nvim
+  stow -t $HOME darwin
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  ln -s "$DOTFILES_DIR/nvim" ~/.config/nvim
-  ln -s "$DOTFILES_DIR/bluetuith" ~/.config/bluetuith
-  ln -s "$DOTFILES_DIR/eww" ~/.config/eww
-  ln -s "$DOTFILES_DIR/rofi" ~/.config/rofi
-
-  # x11
-  ln -s "$DOTFILES_DIR/i3" ~/.config/i3
-  ln -s "$DOTFILES_DIR/picom" ~/.config/picom
-
-  # wayland
-  ln -s "$DOTFILES_DIR/hypr" ~/.config/hypr
-  ln -s "$DOTFILES_DIR/labwc" ~/.config/labwc
-  ln -s "$DOTFILES_DIR/niri" ~/.config/niri
-  ln -s "$DOTFILES_DIR/sway" ~/.config/sway
-
-  ln -s "$DOTFILES_DIR/kanshi" ~/.config/kanshi
-  ln -s "$DOTFILES_DIR/waybar" ~/.config/waybar
-  ln -s "$DOTFILES_DIR/wayfire/wayfire.ini" ~/.config/wayfire.ini
-  ln -s "$DOTFILES_DIR/xdg-desktop-portal" ~/.config/xdg-desktop-portal
-
-  # scripts
-  ln -s "$DOTFILES_DIR/iam" ~/.config/iam
+  stow -t $HOME linux
 fi
+
+stow -t $HOME common
