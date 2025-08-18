b1cb9cfdcdf9da24e39411d87d23f3bb8b685015
ilaumjd <ilaumjd@gmail.com>
2025-08-18 22:04:20 +0700

    stow: update script

--- apply.sh
+++ apply.sh
@@ -2,8 +2,8 @@
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   stow -t $HOME darwin
+  stow -t $HOME common
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
   stow -t $HOME linux
+  stow -t $HOME common
 fi
-
-stow -t $HOME common
