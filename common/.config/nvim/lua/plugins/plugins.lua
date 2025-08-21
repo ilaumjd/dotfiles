2b956c9cc5101557862fc1819d3a4ff7f20f0578
ilaumjd <ilaumjd@gmail.com>
2025-08-21 07:05:33 +0700

    nvim: rose pine

--- common/.config/nvim/lua/plugins/plugins.lua
+++ common/.config/nvim/lua/plugins/plugins.lua
@@ -1,23 +1,6 @@
 return {
   {
     "rose-pine/neovim",
-    name = "rose-pine",
-    opts = {
-      variant = "main", -- auto, main, moon, or dawn
-      dark_variant = "main", -- main, moon, or dawn
-      dim_inactive_windows = false,
-      extend_background_behind_borders = true,
-      enable = {
-        terminal = true,
-        legacy_highlights = true, -- Improve compatibility for bad syntax combinations
-        migrations = true, -- Handle deprecated options automatically
-      },
-      styles = {
-        bold = true,
-        italic = true,
-        transparency = false,
-      },
-    },
   },
 
   {
