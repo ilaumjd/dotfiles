f6fe8b773a55ff912fd7b422808f231270004bd9
ilaumjd <ilaumjd@gmail.com>
2025-08-21 07:34:11 +0700

    nvim: add oil

--- common/.config/nvim/lua/plugins/navigation.lua
+++ common/.config/nvim/lua/plugins/navigation.lua
@@ -1,17 +1,35 @@
 return {
-  "alexghergh/nvim-tmux-navigation",
-  config = function()
-    local nvim_tmux_nav = require("nvim-tmux-navigation")
+  {
+    "alexghergh/nvim-tmux-navigation",
+    config = function()
+      local nvim_tmux_nav = require("nvim-tmux-navigation")
 
-    nvim_tmux_nav.setup({
-      disable_when_zoomed = true, -- defaults to false
-    })
+      nvim_tmux_nav.setup({
+        disable_when_zoomed = true, -- defaults to false
+      })
 
-    vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
-    vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
-    vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
-    vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
-    vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
-    vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
-  end,
+      vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+      vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+      vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+      vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+      vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+      vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+    end,
+  },
+  {
+    "stevearc/oil.nvim",
+    lazy = false,
+    config = function()
+      require("oil").setup({
+        default_file_explorer = true,
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
+        keymaps = {
+          ["q"] = "actions.close",
+        },
+      })
+
+      vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Oil" })
+    end,
+  },
 }
