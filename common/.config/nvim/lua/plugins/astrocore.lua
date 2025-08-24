7cae582f85c411c7b39861b023d0ab514aebc39e
ilaumjd <ilaumjd@gmail.com>
2025-08-24 16:08:08 +0700

    nvim: disable close buffer

--- common/.config/nvim/lua/plugins/astrocore.lua
+++ common/.config/nvim/lua/plugins/astrocore.lua
@@ -71,12 +71,9 @@ return {
           desc = "Close buffer from tabline",
         },
 
-        -- tables with just a `desc` key will be registered with which-key if it's installed
-        -- this is useful for naming menus
-        -- ["<Leader>b"] = { desc = "Buffers" },
-
         -- setting a mapping to false will disable it
-        -- ["<C-S>"] = false,
+        ["<Leader>c"] = false,
+        ["<Leader>C"] = false,
 
         ["x"] = { '"_x', desc = "Delete character without copying to register" },
       },
