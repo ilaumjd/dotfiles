363467c0110e9ecbe97bca7765f44d4de468d0ac
ilaumjd <ilaumjd@gmail.com>
2025-08-24 19:32:27 +0700

    nvim: setup avante

--- common/.config/nvim/lua/plugins/avante.lua
+++ common/.config/nvim/lua/plugins/avante.lua
@@ -1,13 +1,38 @@
+local prefix = "<Leader>a"
 return {
   {
     "yetone/avante.nvim",
+    dependencies = {
+      {
+        "AstroNvim/astrocore",
+        opts = function(_, opts) opts.mappings.n[prefix] = { desc = " Avante" } end,
+      },
+    },
     opts = function(_, opts)
       opts.mappings = vim.tbl_deep_extend("force", opts.mappings or {}, {
         submit = {
           insert = "<C-a>",
         },
+        ask = prefix .. "a", -- <leader>aa
+        edit = prefix .. "e", -- <leader>ae
+        refresh = prefix .. "r",
+        new_ask = prefix .. "n",
+        focus = prefix .. "f",
+        select_model = prefix .. "?",
+        stop = prefix .. "S",
+        select_history = prefix .. "h",
+        toggle = {
+          default = prefix .. "t",
+          debug = prefix .. "d",
+          hint = prefix .. "H",
+          suggestion = prefix .. "s",
+          repomap = prefix .. "R",
+        },
+        files = {
+          add_current = prefix .. ".",
+          add_all_buffers = prefix .. "B",
+        },
       })
-      opts.auto_suggestions_provider = "openai"
     end,
   },
 }
