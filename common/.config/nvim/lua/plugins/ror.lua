2cdbb2acd29ea1c84f4c9932a9f522491e4735fe
ilaumjd <ilaumjd@gmail.com>
2025-08-24 19:45:12 +0700

    nvim: add ror

---
+++ common/.config/nvim/lua/plugins/ror.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "weizheheng/ror.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      { "rcarriga/nvim-notify", optional = true },
+      { "stevearc/dressing.nvim", optional = true },
+    },
+    config = function()
+      require("ror").setup {
+        -- you can customize options here; defaults are fine otherwise
+        test = {
+          message = {
+            file = "Running test file...",
+            line = "Running single test...",
+          },
+          coverage = {
+            up = "DiffAdd",
+            down = "DiffDelete",
+          },
+          notification = {
+            timeout = false,
+          },
+          pass_icon = "✅",
+          fail_icon = "❌",
+        },
+      }
+
+      -- Example of binding the list_commands helper to <leader>rc
+      vim.keymap.set(
+        "n",
+        "<Leader>rc",
+        function() require("ror.commands").list_commands() end,
+        { silent = true, desc = "RoR Menu (ror.nvim)" }
+      )
+    end,
+  },
+}
