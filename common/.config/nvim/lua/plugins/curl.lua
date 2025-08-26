486a0f4df642c7d00dab934d9df6f200bef1c227
ilaumjd <ilaumjd@gmail.com>
2025-08-26 19:47:58 +0700

    nvim: add curl nvim

---
+++ common/.config/nvim/lua/plugins/curl.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "oysandvik94/curl.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("curl").setup {
+        open_with = "buffer",
+        mappings = {
+          execute_curl = "<CR>",
+        },
+      }
+    end,
+  },
+  {
+    "AstroNvim/astrocore",
+    opts = function(_, opts)
+      local maps = opts.mappings
+      -- normal mode mappings
+      maps.n["<leader>cc"] = {
+        function() require("curl").create_global_collection() end,
+        desc = "Create curl collection",
+      }
+      maps.n["<leader>cf"] = {
+        function() require("curl").pick_global_collection() end,
+        desc = "Find curl collection",
+      }
+    end,
+  },
+}
