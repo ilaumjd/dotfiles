f46d83efc2b776bbac248989b65ed99a5c271cd4
ilaumjd <ilaumjd@gmail.com>
2025-08-26 20:55:57 +0700

    nvim: add buffer manager

---
+++ common/.config/nvim/lua/plugins/buffer.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "j-morano/buffer_manager.nvim",
+    config = function() require("buffer_manager").setup {} end,
+  },
+  {
+    "AstroNvim/astrocore",
+    opts = function(_, opts)
+      local maps = opts.mappings
+      maps.n["<Leader>bm"] = {
+        function() require("buffer_manager.ui").toggle_quick_menu() end,
+        desc = "Buffer manager",
+      }
+    end,
+  },
+}
