634504bd66dea7ab2f156696ed9c98a7ea109088
ilaumjd <ilaumjd@gmail.com>
2025-08-28 22:30:09 +0700

    nvim: add mini-ai

--- common/.config/nvim/lua/community.lua
+++ common/.config/nvim/lua/community.lua
@@ -8,6 +8,7 @@ return {
   { import = "astrocommunity.completion.avante-nvim" },
   { import = "astrocommunity.file-explorer.oil-nvim" },
   { import = "astrocommunity.lsp.lspsaga-nvim" },
+  { import = "astrocommunity.motion.mini-ai" },
   { import = "astrocommunity.motion.mini-surround" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.prettier" },
