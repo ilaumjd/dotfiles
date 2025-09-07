ee82f8cadbb5685923961ccf1040e176cf15cf47
ilaumjd <ilaumjd@gmail.com>
2025-09-07 20:02:51 +0700

    nvim: disable treesitter incremental selection if curl

--- common/.config/nvim/lua/plugins/treesitter.lua
+++ common/.config/nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,11 @@ return {
   opts = function(_, opts)
     opts.incremental_selection = {
       enable = true,
+      -- Disable for specific filetypes
+      disable = function(lang, buf)
+        local filetype = vim.bo[buf].filetype
+        return filetype == "curl"
+      end,
       keymaps = {
         init_selection = "<Enter>",
         node_incremental = "<Enter>",
