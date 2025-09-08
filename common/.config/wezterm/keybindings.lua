768f3e172ee4fa5598bf3e89a8214a53a7efc1fc
ilaumjd <ilaumjd@gmail.com>
2025-09-08 22:02:42 +0700

    wezterm: update keybinds

--- common/.config/wezterm/keybindings.lua
+++ common/.config/wezterm/keybindings.lua
@@ -21,6 +21,16 @@ function M.setup(config)
 		leader_key("j", wezterm.action.ActivatePaneDirection("Down")),
 		leader_key("k", wezterm.action.ActivatePaneDirection("Up")),
 		leader_key("l", wezterm.action.ActivatePaneDirection("Right")),
+		leader_key("1", wezterm.action.ActivateTab(0)),
+		leader_key("2", wezterm.action.ActivateTab(1)),
+		leader_key("3", wezterm.action.ActivateTab(2)),
+		leader_key("4", wezterm.action.ActivateTab(3)),
+		leader_key("5", wezterm.action.ActivateTab(4)),
+		leader_key("6", wezterm.action.ActivateTab(5)),
+		leader_key("7", wezterm.action.ActivateTab(6)),
+		leader_key("8", wezterm.action.ActivateTab(7)),
+		leader_key("9", wezterm.action.ActivateTab(8)),
+		leader_key("0", wezterm.action.ActivateTab(9)),
 	}
 	config.leader = { key = "s", mods = "CMD", timeout_milliseconds = 1000 }
 end
