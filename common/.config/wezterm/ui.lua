b99450c12345ff8d776ef69e1b9201c27876d731
ilaumjd <ilaumjd@gmail.com>
2025-09-08 21:14:43 +0700

    wezterm: update ui

--- common/.config/wezterm/ui.lua
+++ common/.config/wezterm/ui.lua
@@ -5,17 +5,14 @@ local M = {}
 function M.setup(config)
 	local scheme = wezterm.get_builtin_color_schemes()["Astrodark (Gogh)"]
 
-	local inactive = {
-		bg_color = scheme.background,
-		fg_color = scheme.foreground,
-	}
-
+	-- Font settings
 	config.default_cursor_style = "BlinkingUnderline"
 	config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 	config.font_size = 14.0
 	config.line_height = 1.2
 
 	-- Window settings
+	config.window_decorations = "TITLE|RESIZE|MACOS_USE_BACKGROUND_COLOR_AS_TITLEBAR_COLOR"
 	config.window_background_opacity = 1.0
 	config.window_padding = {
 		left = 0,
@@ -24,27 +21,34 @@ function M.setup(config)
 		bottom = 0,
 	}
 
-	-- Tab bar settings
-	config.hide_tab_bar_if_only_one_tab = true
-	config.tab_bar_at_bottom = false
-	config.use_fancy_tab_bar = false
-
 	-- Color scheme
 	config.color_schemes = {
 		["scheme"] = scheme,
 	}
 	config.color_scheme = "scheme"
+
+	-- Tab colors
+	local active = {
+		bg_color = scheme.foreground,
+		fg_color = scheme.background,
+	}
+	local inactive = {
+		bg_color = scheme.background,
+		fg_color = scheme.foreground,
+	}
 	config.colors = {
 		tab_bar = {
 			background = scheme.background,
-			active_tab = {
-				bg_color = scheme.foreground,
-				fg_color = scheme.background,
-			},
+			active_tab = active,
 			inactive_tab = inactive,
 			new_tab = inactive,
 		},
 	}
+
+	-- Tab bar settings
+	config.hide_tab_bar_if_only_one_tab = true
+	config.tab_bar_at_bottom = true
+	config.use_fancy_tab_bar = false
 end
 
 return M
