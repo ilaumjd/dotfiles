ed6c434e94adb98f75585f1d17a08b83ab652e02
ilaumjd <ilaumjd@gmail.com>
2025-09-13 16:02:18 +0700

    nvim: update to start oil on start instead of neotree

--- common/.config/nvim/lua/plugins/oil.lua
+++ common/.config/nvim/lua/plugins/oil.lua
@@ -22,6 +22,23 @@ return {
             function() require("oil").open() end,
             desc = "Open Oil",
           }
+          opts.autocmds = require("astrocore").extend_tbl(opts.autocmds or {}, {
+            neotree_start = false,
+            -- Enable Oil to open on directory
+            oil_start = {
+              {
+                event = "BufEnter",
+                desc = "Open Oil on startup with directory",
+                callback = function(args)
+                  local stats = vim.uv.fs_stat(vim.api.nvim_buf_get_name(args.buf))
+                  if stats and stats.type == "directory" then
+                    vim.schedule(function() require("oil").open(vim.api.nvim_buf_get_name(args.buf)) end)
+                    return true
+                  end
+                end,
+              },
+            },
+          })
         end,
       },
     },
