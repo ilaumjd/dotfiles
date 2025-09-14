8402d161a232915f12c918b0fb3acdd254e03644
ilaumjd <ilaumjd@gmail.com>
2025-09-14 16:25:32 +0700

    nvim: @l macro: console.log

--- common/.config/nvim/lua/polish.lua
+++ common/.config/nvim/lua/polish.lua
@@ -2,4 +2,14 @@
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
+-- Disable macro on startup
 vim.cmd "map q <Nop>"
+
+-- Macro: @l
+local esc = vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
+vim.api.nvim_create_augroup("JSLogMacro", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  group = "JSLogMacro",
+  pattern = { "javascript", "typescript" },
+  callback = function() vim.fn.setreg("l", 'yoconsole.log("' .. esc .. 'pa:", ' .. esc .. "pa);" .. esc) end,
+})
