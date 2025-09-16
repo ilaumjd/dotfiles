c94cac95881d3bfb00ad64440eec6d0f8adc9c2a
ilaumjd <ilaumjd@gmail.com>
2025-09-16 08:15:03 +0700

    nvim: add render-markdown

--- common/.config/nvim/lua/community.lua
+++ common/.config/nvim/lua/community.lua
@@ -8,6 +8,7 @@ return {
   { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
   { import = "astrocommunity.completion.avante-nvim" },
   { import = "astrocommunity.file-explorer.oil-nvim" },
+  { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
   { import = "astrocommunity.motion.mini-ai" },
   { import = "astrocommunity.motion.mini-surround" },
   { import = "astrocommunity.pack.bash" },
