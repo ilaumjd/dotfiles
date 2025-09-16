1282738a1d99a3b8ba651c5fa4400608af8c7079
ilaumjd <ilaumjd@gmail.com>
2025-09-16 08:06:15 +0700

    stow: format script

--- apply.sh
+++ apply.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  stow -t $HOME darwin
-  stow -t $HOME common
+  stow -t "$HOME" darwin
+  stow -t "$HOME" common
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  stow -t $HOME linux
-  stow -t $HOME common
+  stow -t "$HOME" linux
+  stow -t "$HOME" common
 fi
