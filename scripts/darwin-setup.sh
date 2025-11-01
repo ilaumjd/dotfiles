cf5991bf31b6197931dddbce9387c4720c475bc1
ilaumjd <ilaumjd@gmail.com>
2025-11-01 15:15:07 +0700

    darwin: setup script

---
+++ scripts/darwin-setup.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+# Keyboard
+/usr/bin/hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x700000029}]}'
+
+# NSGlobalDomain
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
+defaults write NSGlobalDomain ContextMenuGesture -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool true
+
+# Control Center
+defaults write com.apple.menuextra.clock ShowSeconds -bool true
+defaults write com.apple.controlcenter "AirDrop" -int 24
+defaults write com.apple.controlcenter "BatteryShowPercentage" -bool true
+defaults write com.apple.controlcenter "Bluetooth" -int 18
+defaults write com.apple.controlcenter "Display" -int 24
+defaults write com.apple.controlcenter "FocusModes" -int 24
+defaults write com.apple.controlcenter "NowPlaying" -int 24
+defaults write com.apple.controlcenter "Sound" -int 18
+
+# Dock
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float 0.0
+defaults write com.apple.dock autohide-time-modifier -float 0.2
+defaults write com.apple.dock expose-animation-duration -float 0.2
+defaults write com.apple.dock launchanim -bool false
+defaults write com.apple.dock mru-spaces -bool false
+defaults write com.apple.dock orientation -string "right"
+defaults write com.apple.dock show-recents -bool false
+defaults write com.apple.dock show-process-indicators -bool true
+defaults write com.apple.dock static-only -bool true
+defaults write com.apple.dock tilesize -int 48
+defaults write com.apple.dock wvous-br-corner -int 1
+defaults write com.apple.dock wvous-br-modifier -int 0
+
+# Finder
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+defaults write com.apple.finder NewWindowTarget -string "PfHm"
+defaults write com.apple.finder QuitMenuItem -bool true
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+defaults write com.apple.finder _FXSortFoldersFirstOnDesktop -bool true
+
+# Trackpad
+defaults write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
+defaults write NSGlobalDomain com.apple.trackpad.scaling -float 2.0
+defaults write com.apple.AppleMultitouchTrackpad ActuationStrength -int 0
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+defaults write com.apple.AppleMultitouchTrackpad TrackpadRightClick -bool true
+
+# Restart
+killall cfprefsd 2>/dev/null || true
+killall ControlCenter 2>/dev/null || true
+killall Dock 2>/dev/null || true
+killall Finder 2>/dev/null || true
+
+# Misc
+mkdir -p ~/bin
+touch ~/.shrc
