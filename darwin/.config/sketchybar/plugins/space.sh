78d6d4abf344d990d7f8fba1b10177bfc009e262
ilaumjd <ilaumjd@gmail.com>
2025-11-15 00:33:56 +0700

    sketchybar: astrotheme

--- darwin/.config/sketchybar/plugins/space.sh
+++ darwin/.config/sketchybar/plugins/space.sh
@@ -4,4 +4,11 @@
 # the space invoking this script (with name: $NAME) is currently selected:
 # https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
 
-sketchybar --set "$NAME" background.drawing="$SELECTED"
+# Source colors
+source "$CONFIG_DIR/plugins/colors.sh"
+
+if [ "$SELECTED" = "true" ]; then
+  sketchybar --set "$NAME" icon.color=$ACCENT_PRIMARY
+else
+  sketchybar --set "$NAME" icon.color=$TEXT_MUTED
+fi
