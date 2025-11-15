2660f365e46ac4590625071286299bb566e725fb
ilaumjd <ilaumjd@gmail.com>
2025-11-15 19:39:05 +0700

    sketchybar: layout toggle fix

--- darwin/.config/sketchybar/plugins/layout_click.sh
+++ darwin/.config/sketchybar/plugins/layout_click.sh
@@ -6,16 +6,16 @@ LAYOUT=$(yabai -m query --spaces --space | jq -r '.type')
 # Cycle through layouts: stack -> bsp -> float -> stack
 case "$LAYOUT" in
   "stack")
-    yabai -m config layout bsp
+    yabai -m space --layout bsp
     ;;
   "bsp")
-    yabai -m config layout float
+    yabai -m space --layout float
     ;;
   "float")
-    yabai -m config layout stack
+    yabai -m space --layout stack
     ;;
   *)
-    yabai -m config layout stack
+    yabai -m space --layout stack
     ;;
 esac
 
