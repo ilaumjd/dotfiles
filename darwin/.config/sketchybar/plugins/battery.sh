e5b2fb7d34cfc7a425f3a2c8021882f0670e0e01
ilaumjd <ilaumjd@gmail.com>
2025-11-15 15:16:27 +0700

    sketchybar: battery

--- darwin/.config/sketchybar/plugins/battery.sh
+++ darwin/.config/sketchybar/plugins/battery.sh
@@ -7,20 +7,57 @@ if [ "$PERCENTAGE" = "" ]; then
   exit 0
 fi
 
-case "${PERCENTAGE}" in
-  9[0-9]|100) ICON=""
-  ;;
-  [6-8][0-9]) ICON=""
-  ;;
-  [3-5][0-9]) ICON=""
-  ;;
-  [1-2][0-9]) ICON=""
-  ;;
-  *) ICON=""
-esac
-
+# Determine battery icon based on level and charging status
 if [[ "$CHARGING" != "" ]]; then
-  ICON=""
+  # Charging icons (nf-md-battery_charging_*)
+  case "${PERCENTAGE}" in
+    100) ICON="󰂅"  # battery_charging_100
+    ;;
+    9[0-9]) ICON="󰂋"  # battery_charging_90
+    ;;
+    8[0-9]) ICON="󰂊"  # battery_charging_80
+    ;;
+    7[0-9]) ICON="󰢞"  # battery_charging_70
+    ;;
+    6[0-9]) ICON="󰂉"  # battery_charging_60
+    ;;
+    5[0-9]) ICON="󰢝"  # battery_charging_50
+    ;;
+    4[0-9]) ICON="󰂈"  # battery_charging_40
+    ;;
+    3[0-9]) ICON="󰂇"  # battery_charging_30
+    ;;
+    2[0-9]) ICON="󰂆"  # battery_charging_20
+    ;;
+    1[0-9]) ICON="󰢜"  # battery_charging_10
+    ;;
+    *) ICON="󰢜"  # battery_charging_10
+  esac
+else
+  # Discharging icons (nf-md-battery_*)
+  case "${PERCENTAGE}" in
+    100) ICON="󰁹"  # battery_100
+    ;;
+    9[0-9]) ICON="󰂂"  # battery_90
+    ;;
+    8[0-9]) ICON="󰂁"  # battery_80
+    ;;
+    7[0-9]) ICON="󰂀"  # battery_70
+    ;;
+    6[0-9]) ICON="󰁿"  # battery_60
+    ;;
+    5[0-9]) ICON="󰁾"  # battery_50
+    ;;
+    4[0-9]) ICON="󰁽"  # battery_40
+    ;;
+    3[0-9]) ICON="󰁼"  # battery_30
+    ;;
+    2[0-9]) ICON="󰁻"  # battery_20
+    ;;
+    1[0-9]) ICON="󰁺"  # battery_10
+    ;;
+    *) ICON="󰂎"  # battery_alert
+  esac
 fi
 
 # The item invoking this script (name $NAME) will get its icon and label
