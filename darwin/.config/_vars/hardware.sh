3855fed53e89f953d234d5f3c638e04504c3c348
ilaumjd <ilaumjd@gmail.com>
2025-11-16 16:44:06 +0700

    sketchybar: font size

--- darwin/.config/_vars/hardware.sh
+++ darwin/.config/_vars/hardware.sh
@@ -7,6 +7,10 @@ HW_MODEL=$(sysctl -n hw.model)
 
 if [ "$HW_MODEL" == "MacBookAir10,1" ]; then
   BAR_HEIGHT=25
+  ICON_FONT_SIZE=15.0
+  LABEL_FONT_SIZE=12.0
 else
   BAR_HEIGHT=38
+  ICON_FONT_SIZE=17.0
+  LABEL_FONT_SIZE=13.0
 fi
