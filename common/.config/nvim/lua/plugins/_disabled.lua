7c9639c799d69df164b68c37be3aa117521b195d
ilaumjd <ilaumjd@gmail.com>
2026-05-22 08:50:36 +0700

    refactor: move pi agent config to standalone pi-config repo

--- common/.config/nvim/lua/plugins/_disabled.lua
+++ common/.config/nvim/lua/plugins/_disabled.lua
@@ -1,4 +1,5 @@
 return {
-  { "stevearc/aerial.nvim", enabled = false },
-  { "folke/todo-comments.nvim", enabled = false },
+	{ "stevearc/aerial.nvim", enabled = false },
+	{ "folke/todo-comments.nvim", enabled = false },
+	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
