1d52382a7c2dc6b1c31a49254a1c9f06f5bd0e40
ilaumjd <ilaumjd@gmail.com>
2026-06-26 11:41:20 +0700

    sketchybar: add date

--- darwin/.config/sketchybar/items/clock.sh
+++ darwin/.config/sketchybar/items/clock.sh
@@ -4,6 +4,6 @@ sketchybar --add item clock right \
   --set clock update_freq=1 \
   icon=󰥔 \
   icon.color="$ACCENT_PRIMARY" \
-  label.width=65 \
+  label.width=dynamic \
   label.color="$TEXT_PRIMARY" \
   script="$PLUGIN_DIR/clock.sh"
